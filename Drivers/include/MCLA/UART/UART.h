71cfeca7198662fb6ffdcce71122a98049d2ac6a
SamRizk <samir9999life@gmail.com>
2023-05-01 08:34:41 +0300

    new comments reg UART

--- Drivers/include/MCLA/UART/UART.h
+++ Drivers/include/MCLA/UART/UART.h
@@ -136,4 +136,126 @@ extern Uart_enuStatus_t UART_SetTXCallback(Uart_Channel_t uartChannal, CallBack 
 /***************************************************************************************/
 extern Uart_enuStatus_t UART_SetRXCallback(Uart_Channel_t uartChannal,CallBack rx_callback);
 
+
+
+/*
+ * How to configure UART using DMA
+ *
+ *  					-------------
+ * 						| Receiving |
+ * 						-------------
+ * Uart Module
+ * -------------
+ *
+ * ========================== Uart_ConfigType ======================
+ * --> enable the receiving bit (ReceiveEnable) in the Uart configuration
+ * --> enable the Uart
+ * --> Disable all interrupt in case no need for any parity or idle checking
+ * --> Initialize your baudrate
+ *
+ *
+ * Dma Module
+ * -------------
+ *
+ * =======================  Dma_config_t ==========================
+ * --> Enable Stream 5 (for RX UART1)
+ * --> Enable Channel 4 (for RX UART1)
+ * --> Initialize whether you need it circular or not (used with double buffering if you want too)
+ * --> Data direction to Peripheral to memory  {Data_Direction = Per_to_Mem}
+ * --> Memory to be auto increment
+ * --> Peripheral disable auto increment (as you only need to focus on DR register)
+ * --> Initialize the memory size to be 1 byte in case of using 8 bit
+ * --> Initialize the pheripheral size to be 1 byte as well
+ * --> Initialize the priority level required
+ * --> enable the interrupt required as example (Interrupt.Transfer_Complete = Dma_Enable)
+ *
+ *
+ * ======================== Criteria_t =========================
+ *
+ * --> Initialize the Stream 5
+ * --> Initialize the address of your buffer (Memory_0_Address = (u32)MemoryDestination)
+ * --> Initialize the address of your DR Register of the Uart used in our
+ * 		example is UART1 (Peripheral_Address = 0x40011004)
+ *
+ * Initialization
+ * ---------------
+ *
+ * --> Enable Clocks
+ * --> Enable DMA interrupt from NVIC
+ * --> Register the call back function
+ *
+ * ======> Use Uart_ReceiveBufferDma --> to start Receiving
+ * How to configure UART using DMA
+ *
+ *
+ *
+ *
+ *
+ *  					----------------
+ * 						| Transmitting |
+ * 						----------------
+ * Uart Module
+ * -------------
+ *
+ * ========================== Uart_ConfigType ======================
+ * --> enable the Transmitting bit (TransmitEnable) in the Uart configuration
+ * --> enable the Uart
+ * --> Disable all interrupt in case no need for any parity or idle checking
+ * --> Initialize your baudrate
+ *
+ *
+ * Dma Module
+ * -------------
+ *
+ * =======================  Dma_config_t ==========================
+ * --> Enable Stream 7 (for TX UART1)
+ * --> Enable Channel 4 (for TX UART1)
+ * --> Initialize whether you need it circular or not (used with double buffering if you want too)
+ * --> Data direction to Memory to Peripheral  {Data_Direction = Mem_to_Per}
+ * --> Memory to be auto increment
+ * --> Peripheral disable auto increment (as you only need to focus on DR register)
+ * --> Initialize the memory size to be 1 byte in case of using 8 bit
+ * --> Initialize the pheripheral size to be 1 byte as well
+ * --> Initialize the priority level required
+ * --> enable the interrupt required as example (Interrupt.Transfer_Complete = Dma_Enable)
+ *
+ *
+ * ======================== Criteria_t =========================
+ *
+ * --> Initialize the Stream 7
+ * --> Initialize the address of your buffer (Memory_0_Address = (u32)MemoryDestination)
+ * --> Initialize the address of your DR Register of the Uart used in our
+ * 		example is UART1 (Peripheral_Address = 0x40011004)
+ *
+ * Initialization
+ * ---------------
+ *
+ * --> Enable Clocks
+ * --> Enable DMA interrupt from NVIC
+ * --> Register the call back function
+ *
+ * ======> Use Uart_ReceiveBufferDma --> to start Receiving
+ *
+ * */
+
+ 
+/***************************************************************************************/
+/*                                UART_SendBufferDMA     function                      */
+/***************************************************************************************/
+/*  Description: This function to Receive data using DMA                               */
+/* 	input:       Uart_Channel_t ,CallBack                                              */
+/* 	return:      Uart_enuStatus_t                                                      */
+/***************************************************************************************/
+extern Uart_enuStatus_t UART_ReceiveBufferDMA(Uart_Channel_t UartChannel);
+
+
+/***************************************************************************************/
+/*                                UART_SendBufferDMA     function                      */
+/***************************************************************************************/
+/*  Description: This function to send data using DMA                 	               */
+/* 	input:       Uart_Channel_t ,CallBack                                              */
+/* 	return:      Uart_enuStatus_t                                                      */
+/***************************************************************************************/
+extern Uart_enuStatus_t UART_SendBufferDMA(Uart_Channel_t UartChannel);
+
 #endif /* MCLA_UART_UART_H_ */
